1a9eaa98cb218e229787d995419242d65db1239a
denmark0128 <denmarkesmande8737@gmail.com>
2026-01-12 14:15:08 +0800

    added debug fix

--- projectone/settings.py
+++ projectone/settings.py
@@ -25,7 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', True)
+DEBUG = os.getenv("DEBUG", "").lower() == "true"
+
 
 ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
